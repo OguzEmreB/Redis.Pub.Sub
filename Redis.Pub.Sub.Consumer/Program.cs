7d360970edf82328bfcce142ed4647a58ec1bf60
OguzEmre <oguzemre.b@gmail.com>
2024-06-09 14:09:52 +0300

    Redis.Pub.Sub - Channel - Pattern

---
+++ Redis.Pub.Sub.Consumer/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+Console.WriteLine("patternOrChannel : ");  //channel1 , channel1.* , channel1.p1 , channel1.p2
+string patternOrChannel = Console.ReadLine();
+
+await subscriber.SubscribeAsync(patternOrChannel, (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+Console.ReadLine();
---
+++ Redis.Pub.Sub.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+while (true)
+{
+    Console.WriteLine("ChannelPattern : ");  //channel1 , channel1.* , channel1.p1 , channel1.p2
+    string patternOrChannel = Console.ReadLine();
+    Console.WriteLine("Nesaj : ");
+    string message = Console.ReadLine();
+    await subscriber.PublishAsync(patternOrChannel, message);
+
+}
+
+//while (true)
+//{
+//    Console.WriteLine("Nesaj : ");
+//    string message = Console.ReadLine();
+//    await subscriber.PublishAsync("mychannel", message);
+
+//}
